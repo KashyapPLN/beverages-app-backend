38c07adc911c5a07021d32c6f54819da21b2f64b
KashyapPLN <kashyap.pln@gmail.com>
2024-06-11 20:30:28 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express, { response } from "express";
+import { MongoClient } from "mongodb";
+import cors from 'cors';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const MONGO_URL = process.env.MONGO_URL;
+
+const PORT =process.env.PORT;
+
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongo is connected ✌😊");
+  return client;
+}
+
+export const client = await createConnection();
+
+app.get('/',  function (req, res) {
+  res.send('Hello World')
+})
+
+
+
+app.listen(PORT,()=>console.log(`App Started in ${PORT}`));
